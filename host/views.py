ec1d2ce5c88adad58f102d94985cf5a09d8ac264
Erickadikah <erickadikah2030@gmail.com>
2024-03-06 15:51:25 +0300

    update host dashboard

--- host/views.py
+++ host/views.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
-    return render(request, "host.html")
+    users = User.objects.all()
+    return render(request, "host.html", {'users': users})
 
 def user_list(request):
     users = User.objects.all()
-    return render(request, "host/user_list.html", {"users": users})
+    return render(request, "user_list.html", {"users": users})
 
-def host(request):
-    users = User.objects.all()
-    return render(request, 'host.html', {'users': users})
+# def host(request):
+#     users = User.objects.all()
+#     return render(request, 'host.html', )
