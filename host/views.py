2a02515bcc5d9455e4c2f958f895f7b946536778
rencyngina <104766379+rencyngina@users.noreply.github.com>
2024-03-13 22:44:29 +0300

    test code

--- host/views.py
+++ host/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from .forms import ClientForm, GuestLoginForm
 from .models import Client
+from django.contrib.auth.models import User
+import random
+import string
 
 CustomUser = get_user_model()
 
@@ -12,8 +15,10 @@ def index(request):
     clients = Client.objects.all()
     return render(request, "host.html", {'clients': clients})
 
-@csrf_exempt
-@login_required
+def generate_random_password():
+    # Generate a random password of length 8
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+
 @csrf_exempt
 @login_required
 def create_client(request):
@@ -25,14 +30,23 @@ def create_client(request):
     if request.method == 'POST':
         form = ClientForm(request.POST)
         if form.is_valid():
-            # Set the user before saving the form
+    # Set the user before saving the form
             client = form.save(commit=False)
             client.user = request.user  # You can directly assign the request.user
             client.save()
+            password = generate_random_password()
+            user = User.objects.create_user(username=client.email, password=password)
+            print("clint username", client.username)
             print("Client after saving:", client.email)
             print("Client after saving:", client.phoneNumber)
             print("Client after saving:", client.rentPayDate)
             print("Client after saving:", client.rentEndDate)
+            # print("pass", client.password)
+
+            # Set the password directly on the User object
+            user.set_password(password)
+            user.save()
+            print('Generated password:', password)
             # Refresh clients after saving new client
             clients = Client.objects.all()
         return render(request, 'host.html', {'clients': clients})
@@ -40,7 +54,7 @@ def create_client(request):
         form = ClientForm()
     return render(request, 'host.html', {'clients': clients, 'form': form})
 
-
+@csrf_exempt
 def guest_login(request):
     if request.method == 'POST':
         form = GuestLoginForm(request.POST)
@@ -49,12 +63,9 @@ def guest_login(request):
             password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
             if user:
-                if user.has_perm('accounts.can_login_as_guest'):
-                    # Log in the user
-                    login(request, user)
-                    return redirect('/guest/')
-                else:
-                    return HttpResponseBadRequest("You don't have permission to log in as a guest.")
+                # Log in the user
+                login(request, user)
+                return redirect('/guest/')
             else:
                 # Handle invalid credentials
                 return render(request, 'guest_login.html', {'form': form, 'error': 'Invalid username or password'})
